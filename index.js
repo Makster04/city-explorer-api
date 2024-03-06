9e7c99702764bf09cc732a5a5f4b4e1331b8751d
MAK TRNKA <maktrnka@comcast.net>
2024-03-06 14:59:02 -0800

    Changes

--- index.js
+++ index.js
@@ -2,23 +2,15 @@
 
 const express = require('express');
 const dotenv = require('dotenv');
-const cors = require('cors'); // cross origin resource sharing tool
+const cors = require('cors');
 const axios = require('axios');
 const weatherData = require('./data/weather.json');
 dotenv.config();
 
-const PORT =  process.env.PORT || 3000
-
-// Load environment variables from .env file
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
-
-// app.get('/photos,', async (request, response) => {
-  // let searchQuery = request.query.search;
-  // console.log('REQUEST QUERY STRING', request.query.search);
-  // let photoResponse = await axios
-//}
 
 class Forecast {
   constructor(date, description, high, low) {
@@ -36,12 +28,12 @@ app.get('/', (req, res) => {
 app.get('/weather/:lat_lon', (req, res) => {
   const lat = req.params.lat_lon.split('_')[0];
   const lon = req.params.lat_lon.split('_')[1];
-  const foundCity = weatherData.find(city => city.lat === lat && city.lon === lon)
-  console.log("HERE IS WHAT WE KNOW:", lat, lat, foundCity);
-  if (!foundCity || !foundCity.data) { // Check if foundCity or foundCity.data is undefined
+  const foundCity = weatherData.find(city => city.lat === lat && city.lon === lon);
+
+  if (!foundCity || !foundCity.data) {
     return res.status(404).json({ error: 'City not found. Please search for Seattle, Paris, or Amman.' });
   } else {
-    let weatherDex = foundCity.data.map((values) => {
+    let weatherDex = foundCity.data.map(values => {
       return new Forecast(values.datetime, values.weather.description, values.max_temp, values.min_temp);
     });
 
@@ -49,10 +41,7 @@ app.get('/weather/:lat_lon', (req, res) => {
   }
 });
 
-
-const port = process.env.PORT || 3000; // Define port variable
-
-// Other routes and middleware configuration
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
